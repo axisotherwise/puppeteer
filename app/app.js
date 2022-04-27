b59f0b069bdda80483838be222d6851db62faf1d
axis <axisotherwise@gmail.com>
2022-04-27 15:35:12 +0900

    폴더 구조 변경

---
+++ app/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const morgan = require("morgan");
+const path = require("path");
+
+const indexRouter = require("./src/routes/home");
+
+const app = express();
+app.set("port", process.env.NODE_ENV || 1000);
+app.set("view engine", "html");
+nunjucks.configure("./src/views", {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.url} ${req.method} 존재하지 않습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log("1000");
+});
